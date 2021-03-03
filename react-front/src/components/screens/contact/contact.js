9c00ac58b493a3321aa51666754ac9dec541cc54
Aditya Tapshalkar <aditya.taps@gatech.edu>
2021-03-02 19:39:05 -0500

    pushing local history contact page

--- react-front/src/components/screens/contact/contact.js
+++ react-front/src/components/screens/contact/contact.js
@@ -4,6 +4,8 @@ import "./stylesheets/contact.css"
 import bgImg from "../../media/barca-beach.jpg"
 import NavBar from "../../common/elements/navbar";
 import ContentContainer from "../../common/elements/contentContainer";
+import ContactForm from "./components/contactForm";
+import Typist from "react-typist";
 
 function Contact() {
 
@@ -18,6 +20,41 @@ function Contact() {
         <PageWrapper>
             <NavBar />
             <ContentContainer>
+
+                <div className={"contact-content"}>
+                    <div className={"left-content"}>
+
+                        <div className={"contact-typist-container"}>
+                            <Typist startDelay={800} cursor={{blink: true, element: '|'}}>
+                                print("
+                                <b style={{color: 'crimson'}}>
+                                    Let's keep in touch.
+                                </b>
+                                ")
+                            </Typist>
+                        </div>
+
+                        <hr style={{width: "75%"}}/>
+
+                        <div className={"contact-header"}>
+                            Got a project in mind? Let's work together.
+                        </div>
+
+                        <div className={"contact-body"}>
+
+                            I'm a meticulous quick learner and go-getter team player,
+                            and I'm open to collaboration.
+
+                        </div>
+
+
+                    </div>
+                    <div className={"right-content"}>
+
+                        <ContactForm />
+
+                    </div>
+                </div>
 
             </ContentContainer>
         </PageWrapper>
