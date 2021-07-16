a743a920b35ff1839ddf5e0bedb71fc1cfc00ee1
Aditya Tapshalkar <aditya.taps@gatech.edu>
2021-07-15 21:43:21 -0400

    conf files, edited wsgi

--- full-stack/backend/backend/wsgi.py
+++ full-stack/backend/backend/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append("../")
+os.environ.setdefault("PYTHON_EGG_CACHE", "../egg_cache")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
 application = get_wsgi_application()
